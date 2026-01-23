b9dacc2066e95aba9146dc47bc3fc8f521b06b84
DavionCollins <davioncollins3461@gmail.com>
2026-01-23 02:31:40 -0600

    Banner fix 3rd Attempt.

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -56,14 +56,7 @@ const Author = () => {
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
           style={{
-            backgroundImage: `url(${AuthorBanner}) `,
-            backgroundPosition: `center top `,
-            backgroundSize: `cover `,
-            backgroundRepeat: `no-repeat `,
-            height: `360px `,
-            width: `100% `,
-            display: `block `,
-            minHeight: `300px`,
+            backgroundImage: `url(${AuthorBanner}) `
           }}
         ></section>
 
